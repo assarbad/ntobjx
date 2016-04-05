8486683f567a2efc963721d37b681656ee81365e
Oliver Schneider <oliver@assarbad.net>
2016-04-05 21:08:30 +0000

    Adding properties dialog to resources

--- resource.h
+++ resource.h
@@ -5,6 +5,7 @@
 #define IDR_MAINFRAME                   1
 #define IDS_PROGRAM_DESCRIPTION         2
 #define IDS_ABOUT_MENUITEM              3
+#define IDD_PROPERTIES                  107
 #define IDR_POPUP_MENU1                 202
 #define IDD_ABOUT                       205
 #define IDC_STATIC_PROGNAME             1000
@@ -13,6 +14,9 @@
 #define IDC_STATIC_WEBSITE              1003
 #define IDC_STATIC_PROJECT_WEBSITE      1004
 #define IDC_STATIC_REVISION             1005
+#define IDC_EDIT_NAME                   1010
+#define IDC_EDIT_FULLNAME               1011
+#define IDC_EDIT_TYPE                   1012
 #define ID_VIEW_PROPERTIES              32774
 #define ID_POPUPMENU_COPYNAME           32775
 #define ID_POPUPMENU_COPYFULLPATH       32776
@@ -28,9 +32,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        206
+#define _APS_NEXT_RESOURCE_VALUE        207
 #define _APS_NEXT_COMMAND_VALUE         32785
-#define _APS_NEXT_CONTROL_VALUE         1006
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           108
 #endif
 #endif
