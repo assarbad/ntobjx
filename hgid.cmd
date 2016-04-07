5f28423757bacfb546583623cf212e5ec3dde474
Oliver Schneider <oliver@assarbad.net>
2016-04-07 19:54:00 +0000

    Adding version info string containing the URL to browse the exact revision from which this was built

--- hgid.cmd
+++ hgid.cmd
@@ -3,5 +3,6 @@ setlocal ENABLEEXTENSIONS & pushd .
 set HGTIPFILE="%~dp0hgid.h"
 for /f %%i in ('hg id -i') do @echo #define HG_REV_ID "%%i"> %HGTIPFILE%
 for /f %%i in ('hg id -n') do @echo #define HG_REV_NO %%i>>  %HGTIPFILE%
+for /f %%i in ('hg log -l 1 -r . -T {node}') do @echo #define HG_FULLID %%i>>  %HGTIPFILE%
 if exist %HGTIPFILE% type %HGTIPFILE%
 popd & endlocal & goto :EOF
